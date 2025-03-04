da6f04dc598e3d780ff4f098241a2451bf9c8a6b
GAVESH BATHAM <gaveshbatham1@gmail.com>
2025-03-04 19:41:46 +0530

    database setup

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from "./config/db.js"
 
 dotenv.config();
-
+connectDB();
 const app= express();
 const PORT= process.env.PORT || 5000
 
 
 
 app.get('/', (req,res)=>{
-    res.send("<h1> this is from backend  </h1>")
+    res.send("<h1> this is from backend  </h1>") 
 })
 
 app.listen(PORT, ()=>{

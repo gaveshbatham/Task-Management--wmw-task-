f4ab1a5762d6aaf6ddd4da6742ed5ef33288d27b
GAVESH BATHAM <gaveshbatham1@gmail.com>
2025-03-06 15:24:01 +0530

    user api done

---
+++ backend/model/user.js
@@ -0,0 +1,33 @@
+
+import mongoose from "mongoose";
+
+const userSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },role: {
+        type: String,
+        enum: ["admin", "user"], // Only "admin" or "user" roles
+        default: "user",
+      },
+      profilePhoto: {
+        data: Buffer, 
+        contentType: String, 
+      },
+    },
+    { timestamps: true }
+)
+
+
+const Users = mongoose.model("users",userSchema)
+
+export default Users;
---
+++ backend/routers/task.js
@@ -0,0 +1,25 @@
+import express from 'express'
+
+
+const router=express.Router();
+
+// router.get('/', )
+
+
+// router.post('/add', )
+
+
+
+// router.put('/update/:email', )
+
+
+
+
+// router.delete('/delete/:email', )
+
+
+
+
+
+export default router;
+

c329a6eac19ec3af88a6c700e9a1e36851c0fee3
himansh-01 <himanshprajapati2004@gmail.com>
2025-03-08 13:00:58 +0530

    Api integration

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -4,7 +4,14 @@ import  Sidebar  from '@/components/Sidebar'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
-const page = () => {
+const page = async() => {
+  // const response = await fetch(('http://localhost:5000/task/add'),{
+  //   method: "POST",
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(),
+  // })
   return (
     <div className='w-[100%] bg-gray-100'>
       <Main />
